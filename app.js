cd12b4bf6e95887b68c3c6d697cf6596896e9ff3
roibh <roi@ewave.co.il>
2016-08-22 22:38:06 +0300

    static

--- app.js
+++ app.js
@@ -18,38 +18,55 @@ var cache = require("@nodulus/cache");
 var EventEmitter = require('events').EventEmitter;
 global.eventServer = new EventEmitter();
 global.cache = cache;
-var app = express();
-
-
-var http = require("http").createServer(app);
-var server = require('http').Server(app);
-webServer.start(server, app, function () {
-    // var socket = require('socket.io')(server);
-    // var io = socket.listen(server);
-    // 
-    // console.log("socket listen on:" + io)
-    // SocketUse(io);
-});
 
 
 
-var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
-app.use(bodyParser.json({
-    reviver: function (key, value) {
-        var match;
-        if (typeof value === "string" && (match = value.match(regexIso8601))) {
-            var milliseconds = Date.parse(match[0]);
-            if (!isNaN(milliseconds)) {
-                return new Date(milliseconds);
+var server = null;require('http')
+var app = null;
+if (global.nodulus_app) {
+    app = global.nodulus_app;
+    server = global.nodulus_server;
+}
+else {
+    app = express();
+    global.nodulus_app = app;
+
+
+
+
+    var http = require("http").createServer(app);
+    server = require('http').Server(app);
+    global.nodulus_server = server;
+    webServer.start(server, app, function () {
+        // var socket = require('socket.io')(server);
+        // var io = socket.listen(server);
+        // 
+        // console.log("socket listen on:" + io)
+        // SocketUse(io);
+    });
+
+
+
+    var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+    app.use(bodyParser.json({
+        reviver: function (key, value) {
+            var match;
+            if (typeof value === "string" && (match = value.match(regexIso8601))) {
+                var milliseconds = Date.parse(match[0]);
+                if (!isNaN(milliseconds)) {
+                    return new Date(milliseconds);
+                }
             }
-        }
-        return value;
-    },
-    limit: '50mb',
-}));
+            return value;
+        },
+        limit: '50mb',
+    }));
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+}
+
+
 
 module.exports = app;
 module.exports.static = express.static;
