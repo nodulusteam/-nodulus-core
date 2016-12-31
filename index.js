dde02794df069531f4556cddd7eeefcc9a67b276
roibh <roi@ewave.co.il>
2016-12-31 11:32:21 +0200

    implement better signleton design

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const config = require('@nodulus/config');
-var mode = config.appSettings.framework || 'express';
-var selectedCore = require('./lib/' + mode);
+var selectedCore = null;
+if (global.nodulus && global.nodulus.core) {
+    selectedCore = global.nodulus.core;
+}
+else {
+    const config = require('@nodulus/config');
+    var mode = config.appSettings.framework || 'express';
+    global.nodulus.core = selectedCore = require('./lib/' + mode);
+}
 module.exports = selectedCore;
